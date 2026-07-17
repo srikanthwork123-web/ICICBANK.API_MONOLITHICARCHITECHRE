f5cf6bfd0f6d15ef4f17c364d274490379279121
Dell <Dell@DESKTOP-13B42NJ>
2026-07-17 08:50:07 +0530

    Notes Updated

--- ICICBANK.API_MONOLITHICARCHITECHRE/Program.cs
+++ ICICBANK.API_MONOLITHICARCHITECHRE/Program.cs
@@ -46,4 +46,43 @@ A)
 =>Monolithic architecture menas if you develop any project,they mainteined only one api project for apis development and only one database used.single api project and single database used for entire project development.
 =>If any application server is down ,entire production application  will not work.this is one main drawback in monolithic architecture.
 
+2.what is microservices architecture?can you explain in detail?
+A)
+=>Microservices architecture means if you develop any project,they mainteined multiple api projects for apis development and multiple databases used.
+=>single api project and single database used for each module development.
+=>we need to deploy each microservice separetly in azure cloud or any other cloud platform.
+Advantages of Microservices Architecture in .NET Core
+✅ Scalability – Scale services independently based on demand.
+✅ Flexibility – Develop using different technologies and databases per service.
+✅ Resilience – Failure in one service doesn’t crash the entire application.
+✅ Faster Deployment – Deploy and update services individually.
+✅ Better Maintainability – Easier to manage and update small services.
+✅ Cloud-Friendly – Works well with containers like Docker and Kubernetes.
+Disadvantages
+❌ Complexity – Managing multiple services is harder than a monolith.
+❌ Data Management – Handling distributed databases and transactions is tricky.
+❌ Latency – Network calls between services can slow performance.
+❌ Monitoring Challenges – Requires robust logging and monitoring tools.
+❌ Deployment Overhead – More infrastructure setup is needed.
+
+ */
+
+/*
+ To implement microservices Architecture for each Api microservice need to create separate databases
+
+=================================================================
+
+Create database ICICBANKOverview
+
+Create database ICICBANKBankAccounts
+
+Create database ICICBANKPaymentsAndTransfer
+
+Create database ICICBANKCardsAndLoans
+
+Create database ICICBANKInvestmentsAndInsurance
+
+Create database ICICBANKCustomerService
+
+=============================== 
  */
